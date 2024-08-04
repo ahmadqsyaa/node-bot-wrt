2434d95163968d06b748a8583c8209f4c4f71d10
Rickk!! <114840842+ahmadqsyaa@users.noreply.github.com>
2024-08-04 21:47:09 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-wget -O /usr/bin/ht-api https://raw.githubusercontent.com/ahmadqsyaa/node-bot-wrt/master/lib/ht-api
-wget -O /usr/bin/mmsms https://raw.githubusercontent.com/ahmadqsyaa/node-bot-wrt/master/lib/mmsms
+mv ~/node-bot-wrt/lib/ht-api /usr/bin
+mv ~/node-bot-wrt/lib/mmsms /usr/bin
 chmod 755 /usr/bin/ht-api
 chmod 755 /usr/bin/mmsms
 chmod 755 /lib/*.sh
